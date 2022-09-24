11c391f18109fa53eee5c5d94418c1b55a0fcba3
Denis Ignatov <dsignatov@licard.com>
2022-09-24 08:56:01 +0300

    Initial commit

---
+++ src/main/java/com/denisspec/adcmodule/AdcmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.denisspec.adcmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdcmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdcmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denisspec/adcmodule/AdcmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisspec.adcmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdcmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
